4059d2fb0676cb07c7ccdf4ea5f8f6293e1eb3c8
Tessa Thornton <tessathornton@gmail.com>
2017-03-27 21:54:21 -0400

    communismed

--- overrides.js
+++ overrides.js
@@ -5,10 +5,13 @@
   function nationalize(textNode){
     if(!textNode) return;
     var oldValue = textNode.textContent;
-    var newValue = oldValue
-      .replace(/like[sd]?/,"communism");
-    if (newValue != oldValue) {
-      textNode.textContent = newValue;
+    var match = oldValue.match(/like([sd]?)/);
+    if (match) {
+      var suffix = match[1] === 'd' ? 'ed' : match[1];
+      var newValue = oldValue.replace(/like[sd]?/, 'communism' + suffix);
+      if (newValue != oldValue) {
+        textNode.textContent = newValue;
+      }
     }
   }
 
@@ -41,7 +44,7 @@
         return node.nodeType === Node.TEXT_NODE;
       }).forEach(nationalize);
     });
-    
+
     //can't rely on css alone becauese `content` property is not overrideable
     $(".Icon--heartBadge").forEach(function(el){
       el.classList.remove("Icon--heartBadge");
